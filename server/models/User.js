6315343c90e2d1fb0913e89ec1aa86dd7f101fbf
Avalynn Judge <avalynnjudge@gmail.com>
2022-04-20 19:23:14 -0700

    fixed typedefs and resolvers for Post model

--- server/models/User.js
+++ server/models/User.js
@@ -18,7 +18,13 @@ const userSchema = new Schema({
     type: String,
     required: true,
     minlength: 5,
-  }
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+  ],
 });
 
 userSchema.pre('save', async function (next) {
